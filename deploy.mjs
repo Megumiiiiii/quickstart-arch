3c14994dd4e1c8318f5f1440760460582122d32a
megumii <contact@megumii.xyz>
2023-11-14 14:59:32 +0700

    Update deploy.mjs

--- deploy.mjs
+++ deploy.mjs
@@ -1,27 +1,30 @@
-import Irys from "@irys/sdk";
+import Bundlr from "@bundlr-network/client";
 import { WarpFactory, defaultCacheOptions } from "warp-contracts";
+import fs from "fs";
 import Arweave from "arweave";
 
 const ANT = "C7hd7ZrANhot0NveDPg8pu75uOQkn5qWbvFyVLUTjc8";
-const DEPLOY_FOLDER = "./src/.vuepress/dist";
-const IRYS_NODE = "https://node2.irys.xyz";
-
-const jwk = JSON.parse(Buffer.from(process.env.PERMAWEB_KEY, "base64").toString("utf-8"));
 const arweave = Arweave.init({ host: "arweave.net", port: 443, protocol: "https" });
-const irys = new Irys({ url: IRYS_NODE, token: "arweave", key: jwk });
+//const jwk = JSON.parse(fs.readFileSync('../wallet.json', 'utf-8'))
+const jwk = JSON.parse(Buffer.from(process.env.PERMAWEB_KEY, "base64").toString("utf-8"));
+
+const bundlr = new Bundlr.default("https://node2.irys.xyz", "arweave", jwk);
 const warp = WarpFactory.custom(arweave, defaultCacheOptions, "mainnet").useArweaveGateway().build();
 
 const contract = warp.contract(ANT).connect(jwk);
 // upload folder
-const result = await irys.uploadFolder(DEPLOY_FOLDER, {
+const result = await bundlr.uploadFolder("./src/.vuepress/dist", {
 	indexFile: "index.html",
 });
 
 // update ANT
+
 await contract.writeInteraction({
 	function: "setRecord",
 	subDomain: "@",
 	transactionId: result.id,
 });
+
+console.log("Deployed Cookbook, please wait 20 - 30 minutes for ArNS to update!");
 
 console.log("Deployed Cookbook, please wait 20 - 30 minutes for ArNS to update!");
